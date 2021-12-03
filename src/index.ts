bca965efee2a20c517150e338036906d10101a61
Tawfiek <>
2021-12-03 02:00:30 +0200

    core: add server setup and required packages

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+  res.send('Your server is running on port: ' + port);
 });
 
 app.listen(port, () => {
